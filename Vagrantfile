dcd017107f4fb6fc4275b1fc76069e505e0a3125
Beni vagenfeld <beni.vd@gmail.com>
2015-02-11 15:54:57 +0200

    new

--- Vagrantfile
+++ Vagrantfile
@@ -5,6 +5,7 @@ VAGRANTFILE_API_VERSION = "2"
 Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
 	config.vm.box = "hashicorp/precise64"
 	config.vm.network :forwarded_port, guest: 7990, host: 7990
+	config.vm.network :forwarded_port, guest: 8080, host: 8080
     config.vm.network :private_network, ip: "192.168.35.15"
 	
 	config.vm.provider "virtualbox" do |v|
--- deployStash.sh
+++ deployStash.sh
@@ -27,4 +27,8 @@ sudo update-rc.d stash defaults
 #####Starting the service for the current session virtualbox(before reboot)######
 service stash start
 
-
+####Installing jenkins####
+wget -q -O - https://jenkins-ci.org/debian/jenkins-ci.org.key | sudo apt-key add -
+sudo sh -c 'echo deb http://pkg.jenkins-ci.org/debian binary/ > /etc/apt/sources.list.d/jenkins.list'
+sudo apt-get update
+sudo apt-get install jenkins
