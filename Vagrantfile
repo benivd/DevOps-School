b1254a96feb0b13ad7577d2e81669c01acf9c961
Beni vagenfeld <beni.vd@gmail.com>
2015-02-11 15:29:02 +0200

    fix #27 - I set internal stash server including all the configuration that need for that (network,etc...).

--- Vagrantfile
+++ Vagrantfile
@@ -1,19 +1,16 @@
 # -*- mode: ruby -*-
 # vi: set ft=ruby :
 VAGRANTFILE_API_VERSION = "2"
-$bootstrap = <<SCRIPT
-PATH=$PATH:/vagrant
-export PATH
-cd /vagrant
-atlassian_stash_up.sh
-SCRIPT
 
 Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
 	config.vm.box = "hashicorp/precise64"
+	config.vm.network :forwarded_port, guest: 7990, host: 7990
+    config.vm.network :private_network, ip: "192.168.35.15"
+	
 	config.vm.provider "virtualbox" do |v|
 		v.memory = 1024
 		v.cpus = 2
 	end 
-	config.vm.provision "shell", inline: $bootstrap
+	config.vm.provision "shell", path: "deployStash.sh"
   
 end
---
+++ deployStash.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+sudo apt-get update
+
+#####Packages for using the last version of git and stash######
+sudo apt-get -y install curl openjdk-7-jre openjdk-7-jdk software-properties-common python-software-properties
+sudo add-apt-repository ppa:git-core/ppa -y
+sudo apt-get update
+sudo apt-get -y install git
+
+#####Stash Install...######
+curl -OL http://www.atlassian.com/software/stash/downloads/binary/atlassian-stash-3.6.1.tar.gz
+tar xz -C /opt -f atlassian-stash-3.6.1.tar.gz
+sudo mkdir /opt/stash-home
+STASH_HOME=/opt/stash-home
+export STASH_HOME
+
+#####Setting user that will run the stash service on startup######
+sudo groupadd stash
+sudo useradd -m -s /bin/bash -g stash stash
+echo stash:stash | sudo chpasswd
+sudo chown stash.stash -R /opt/atlassian-stash-3.6.1
+sudo chown stash.stash -R /opt/stash-home
+sudo cp /vagrant/stash /etc/init.d
+sudo chmod  755 /etc/init.d/stash
+sudo update-rc.d stash defaults
+
+#####Starting the service for the current session virtualbox(before reboot)######
+service stash start
+
+
